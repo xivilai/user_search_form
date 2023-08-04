ae739732c1dbf8bd26d21d6ac2979c5d9874bcd3
Slava Logos <logos.slava@gmail.com>
2023-08-04 18:38:56 +0300

    add validation and phone mask

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,72 @@
-import { useQuery } from "@tanstack/react-query";
-import "./App.css";
+import React from "react";
+import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { FormEvent, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Input from "react-input-mask";
+
+import "./App.css";
+
+interface Inputs {
+  email: string;
+  phone: string;
+}
+
+const searchUsers = async (formData: Inputs) => {
+  let url = `/api/users?email=${formData.email}`;
+  if (formData.phone) {
+    url += `&phone=${formData.phone}`;
+  }
+
+  const response = await axios.get(url);
+
+  return response.data();
+};
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-
   const {
-    data: users,
-    isLoading,
-  } = useQuery({
-    queryKey: ["users"],
-    queryFn: () => axios.get(`/users?email=${email}`),
-    refetchOnWindowFocus: false
-  });
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
+  const mutation = useMutation(searchUsers);
 
-    const form = evt.target as HTMLFormElement;
-    const email: string = form.email;
-    const phoneNumber: string = form.phoneNumber;
+  const handleSearch: SubmitHandler<Inputs> = (formData) => {
+    mutation.mutate(formData);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form action="/api/users" onSubmit={handleSubmit(handleSearch)}>
       <div>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          autoComplete="email"
-          required
-        />
-        <input
-          type="number"
-          name="phone-number"
-          autoComplete="off"
-          placeholder="phone number"
-          required
-        />
+        <label>
+          Email{" "}
+          <input
+            type="email"
+            {...register("email", {
+              required: "Email address is required",
+              pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            })}
+          />
+        </label>
+
+        {errors.email && <p role="alert">{errors.email.message}</p>}
       </div>
 
-      <button type="submit" disabled={isLoading}>
-        {isLoading ? "Loading..." : "Search users"}
+      <div>
+        <label>
+          Phone{" "}
+          <Input
+            mask="99-99-99"
+            {...register("phone", { pattern: /^\d{2}-\d{2}-\d{2}$/ })}
+          />
+        </label>
+        {errors.phone && (
+          <p role="alert">Phone must be in the format ##-##-##</p>
+        )}
+      </div>
+
+      <button type="submit" disabled={mutation.isLoading}>
+        {mutation.isLoading ? "Loading..." : "Search users"}
       </button>
     </form>
   );
